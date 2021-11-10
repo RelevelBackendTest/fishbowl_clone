ae77008ee0d597db454ccbc8ad6a0d9c32b1af5a
RelevelBackendTest <87223414+RelevelBackendTest@users.noreply.github.com>
2021-11-10 20:53:07 +0530

    Add files via upload

---
+++ src/test/java/com/relevel/backend/fishbowl/FishbowlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relevel.backend.fishbowl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishbowlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
